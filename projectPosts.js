3652257c5a38848823be4b15df9e41ad3925bc8c
MaximStockmans <contact@maximstockmans.com>
2021-01-17 23:02:20 +0100

    add javascript html css

---
+++ projectPosts.js
@@ -0,0 +1,35 @@
+function makeList() {
+    // Establish the array which acts as a data source for the list
+    let listData = [
+        ['project1.html','CNC with Arduino (Python, C++)','images/image-placeholder.png']
+    ];
+    let numberOfListItems = listData.length;
+    let element = document.getElementById('projectsRow');
+    for (i = 0; i < numberOfListItems; i++) {
+            var column = document.createElement('div');
+            column.setAttribute('class', 'col-md-4 project');
+            
+            var a = document.createElement('a');
+            a.setAttribute('href', listData[i][0]);
+
+            var projectTitle = document.createElement('div');
+            projectTitle.setAttribute('class', 'centerH');
+            var title = document.createElement('h3');
+            title.innerHTML = listData[i][1];
+            projectTitle.appendChild(title);
+            
+            var image = document.createElement('img');
+            image.setAttribute('src', listData[i][2]);
+            image.setAttribute('class', 'img-responsive');
+            image.setAttribute('width', '100%');
+            
+            a.appendChild(projectTitle);
+            a.appendChild(image);
+            column.appendChild(a);
+        
+            element.appendChild(column);
+    }
+}
+
+// Usage
+makeList();
