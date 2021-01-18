579670b3f7e83f47a782eabd94a9cf7f4a678e98
MaximStockmans <contact@maximstockmans.com>
2021-01-18 09:42:12 +0100

    add project3

--- projectPosts.js
+++ projectPosts.js
@@ -2,7 +2,8 @@ function makeList() {
     // Establish the array which acts as a data source for the list
     let listData = [
         ['project1.html','CNC with Arduino (Python, C++)','images/image-placeholder.png'],
-        ['project2.html','Earth2 Bot using Selenium (Java)','images/image-placeholder.png']
+        ['project2.html','Earth2 Bot using Selenium (Java)','images/image-placeholder.png'],
+        ['project3.html','Personal Website (JavaScript, HTML, CSS)','images/image-placeholder.png']
     ];
     let numberOfListItems = listData.length;
     let element = document.getElementById('projectsRow');
